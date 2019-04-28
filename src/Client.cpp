39c729b1c30fda9d5ea2af585f6b6900cc427cb7
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-04-28 16:39:16 -0700

    Updated types

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,7 +2,7 @@
 #include <boost/asio.hpp>
 
 int main() {
-    boost::asio::io_context io;
+    boost::asio::io_service io;
     boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
     t.wait();
 
