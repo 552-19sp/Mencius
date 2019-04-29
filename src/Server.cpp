f70fae9d9ff63971749e7614fa82f4ea50a29124
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-04-28 17:34:43 -0700

    Added tasks

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,4 +2,16 @@
 
 int main() {
     std::cout << "Server" << std::endl;
+
+
+    /*
+        - Accept list of servers from command line.
+        - Ping every server (heartbeat)
+        - Implement KV Store
+            * At most once semantics
+        - Implement PMMC (assume no failures)
+            * Async
+        - Handle failure case
+        - Set up integration tests
+    */
 }
