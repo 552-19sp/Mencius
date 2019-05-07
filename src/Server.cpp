d05d26a650c99baf4fc34383b9c8cc596679c29b
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-05-07 14:11:57 -0700

    Added async timer example

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+void print(const boost::system::error_code &/*e*/,
+        boost::asio::deadline_timer *t, int *count) {
+    if (*count < 5) {
+        std::cout << *count << std::endl;
+        ++(*count);
+
+        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+
+        t->async_wait(boost::bind(print, boost::asio::placeholders::error, t, count));
+    }
+}
 
 int main() {
-    std::cout << "Server" << std::endl;
+    std::cout << "Server starting..." << std::endl;
 
+    boost::asio::io_service io;
 
-    /*
-        - Accept list of servers from command line.
-        - Ping every server (heartbeat)
-        - Implement KV Store
-            * At most once semantics
-        - Implement PMMC (assume no failures)
-            * Async
-        - Handle failure case
-        - Set up integration tests
-    */
+    int count = 0;
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
+
+    t.async_wait(boost::bind(print, boost::asio::placeholders::error, &t, &count));
+
+    // This blocks until all the work is done.
+    io.run();
+
+    std::cout << "Final count is " << count << std::endl;
+
+    return 0;
 }
