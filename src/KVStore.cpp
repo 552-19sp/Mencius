119fa97c9c8cacb620d35f53c59de3a08b022571
Ubuntu <jack.khuu.7@gmail.com>
2019-05-14 17:19:45 +0000

    Checking changes to local config and moved KV into folder

--- src/KVStore.cpp
+++
@@ -1,7 +0,0 @@
-
-
-class 
-
-int main() {
-  return 0;
-}
---
+++ src/util/KVStore.cpp
@@ -0,0 +1,15 @@
+# include <map>
+
+using namespace std;
+
+class AMOStore {
+  private:
+    map<T, T> store;
+
+  public:
+
+} 
+
+int main() {
+  return 0;
+}
