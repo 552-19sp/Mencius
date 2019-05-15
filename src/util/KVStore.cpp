d6a5fab012e2fb6f51ab527c3157e50d4d7c46d3
Ubuntu <jack.khuu.7@gmail.com>
2019-05-15 06:27:59 +0000

    POC done, needs cleaning

--- src/util/KVStore.cpp
+++ src/util/KVStore.cpp
@@ -5,41 +5,58 @@
 
 using namespace std;
 
-template <class S, class T>
+enum Action { PUT, APPEND, GET };
+
+// Supports an integer Key and a String value
 class KVStore {
   private:
-    map<S, T> _store;
+    map<int, string> _store;
 
 
   public:
-    void insert(S key, T value) {
-      if (_store.find(key) == _store.end()) {
-	_store[key] = value;
-      } else {
-        _store[key] = value + _store[key];  
+    string insert(int key, string value) {
+      if (_store.find(key) != _store.end()) {
+        exit(EXIT_FAILURE);
       }
+      _store[key] = value;
+      
+      return _store[key];
     }
 
-    T get(S key) {
+    string append(int key, string value) {
+      if (_store.find(key) == _store.end()) {
+        exit(EXIT_FAILURE);
+      }
+      _store[key] = _store[key] + value;  
+      return _store[key];
+    }
+
+    string get(int key) {
       return _store[key];
     }
 };
 
-// Currently used only to add values
+
 class AMOCommand {
   private:
     int _seqNum;
     int _key;
-    int _value;
+    string _value;
+    Action _action;
     
 
   public:
     AMOCommand() {}
 
-    AMOCommand(int seqNum, int key, int value) {
+    AMOCommand(int seqNum, int key, string value, Action action) {
       _seqNum = seqNum;
       _key = key;
       _value = value;
+      _action = action;
+    }
+
+    bool operator< (const AMOCommand& r) const {
+      return this->_seqNum < r._seqNum;
     }
 
     int getSeqNum() {
@@ -50,68 +67,108 @@ class AMOCommand {
       return _key;
     }
 
-    int getValue() {
+    string getValue() {
       return _value;
+    }
+
+    Action getAction() {
+      return _action;
     }
 };
 
-template <class T>
+
 class AMOResponse {
   private:
     AMOCommand _command;
-    T _value;
-    
+    string _value; 
       
   public:
     AMOResponse() {}
 
-    AMOResponse(AMOCommand command, T value) {
+    AMOResponse(AMOCommand command, string value) {
       _command = command;
-      _value = value;
+      _value = value; 
+    }
+
+    bool operator< (const AMOResponse& r) const {
+      return _command < r._command;
     }
 
     AMOCommand getCommand() {
       return _command;
     }
 
-    T getValue() {
+    string getValue() {
       return _value;
     }
 };
 
-template <class S, class T>
+
 class AMOStore {
   private:
-    KVStore<S, T> kvStore;
-    map<AMOCommand, AMOResponse<T>> prev;
+    KVStore kvStore;
+    map<AMOCommand, AMOResponse> prev;
 
   public:
     AMOStore() {
-      kvStore = KVStore<S, T>();
+      kvStore = KVStore();
     }
 	  
-    AMOResponse<T> execute(AMOCommand command) {
+    AMOResponse execute(AMOCommand command) {
+      string s;
       if (!alreadyExecuted(command)) {
-        kvStore.insert(command.getKey(), command.getValue());
-	prev[command] = AMOResponse<T> (command, command.getValue());
-      }
+	cout << "New" << endl;
+	switch(command.getAction()) {
+	  case PUT:
+	    cout << "PUT" << endl;
+            s = kvStore.insert(command.getKey(), command.getValue());
+	    break;
+	  case APPEND:
+	    cout << "APPEND" << endl;
+            s = kvStore.append(command.getKey(), command.getValue());
+	    break;
+	  case GET:
+	    cout << "GET" << endl;
+            s = kvStore.get(command.getKey());
+	    break;
+	  default:  
+	    exit(EXIT_FAILURE);
+	}
 
+	prev[command] = AMOResponse(command, s);
+      }
+      cout << "``````````Returned from Execute````````````````" << endl;
+      cout << prev[command].getValue() << endl;
+      cout << "```````````````````````````````````````````````" << endl;
       return prev[command];
     }
 
     bool alreadyExecuted(AMOCommand command) {
-      return prev.find(command) == prev.end();
+      return prev.find(command) != prev.end();
     }
 };
 
-int main() {
-  AMOStore<int, int> app = AMOStore<int, int>();
 
-  AMOCommand c1 = AMOCommand(0, 10, 1);
-  AMOCommand c2 = AMOCommand(0, 20, 10);
-  AMOCommand c3 = AMOCommand(0, 10, 100);
-  AMOCommand c4 = AMOCommand(0, 20, 1000);
-  AMOCommand c5 = AMOCommand(0, 30, 10000);
+int main() {
+  AMOStore app = AMOStore();
+
+  AMOCommand c1 = AMOCommand(0, 10, "1", PUT);
+  AMOCommand c1g = AMOCommand(1, 10, "1", GET);
+  AMOCommand c2 = AMOCommand(2, 20, "10", PUT);
+  AMOCommand c1a = AMOCommand(3, 10, "100", APPEND);
+  AMOCommand c1a2 = AMOCommand(4, 10, "100", APPEND);
+  AMOCommand c1g2 = AMOCommand(5, 10, "100", GET);
+  AMOCommand c2a = AMOCommand(6, 20, "1000", APPEND);
+  AMOCommand c3 = AMOCommand(6, 30, "10000", PUT);
+
+  app.execute(c1);
+  app.execute(c1g);
+  app.execute(c2);
+  app.execute(c1a);
+  app.execute(c1a2);
+  app.execute(c1g2);
+  app.execute(c2a);
+  app.execute(c3);
   
   return 0;
 }
