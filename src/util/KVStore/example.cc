26c38c188bad8f47f5484cc9903ac403c7283c89
Ubuntu <jack.khuu.7@gmail.com>
2019-05-20 20:58:32 +0000

    Adding example execution

---
+++ src/util/KVStore/example.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "./AMOStore.h"
+#include "./AMOCommand.h"
+#include "./Action.h"
+
+using namespace Action;
+using namespace std;
+
+int main() {
+  AMOStore::AMOStore app = AMOStore::AMOStore();
+
+  AMOCommand::AMOCommand c1 = AMOCommand::AMOCommand(0, 10, "A", PUT);
+  app.execute(c1);
+  cout << "Done" << endl;
+  return 0;
+}
