d75a698273d2318ae6f4e8ede8dc0bb65db9bc15
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-05-21 15:15:49 -0700

    Move some client code to shared Utilities class (#36)

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -159,7 +159,13 @@ int main(int argc, char **argv) {
   }
 
   auto server_addresses = Utilities::ReadConfig(kConfigFilePath);
-  std::cout << server_addresses.size() << std::endl;
+
+  auto parsed_ops = Utilities::ParseOperations(argv[1]);
+  // For now, print ops to cout. Eventually, they will be printed
+  // to an output file after completion.
+  for (int i = 0; i < parsed_ops.size(); i++) {
+    std::cout << parsed_ops[i] << std::endl;
+  }
 
   boost::asio::io_context io_context;
   tcp::resolver r(io_context);
@@ -169,94 +175,3 @@ int main(int argc, char **argv) {
 
   io_context.run();
 }
-
-/*
-// Assume that each line in our config is <= 255 bytes.
-const int MAX_CONFIG_LINE_LEN = 255;
-
-// Assume that amount of data read from socket <= 128 bytes.
-const int MAX_SOCKET_DATA_LEN = 128;
-
-// Location of config file storing host and port pairs.
-const char *CONFIG_FILE_PATH = "config";
-
-int main(int argc, char* argv[]) {
-  try {
-    // Read list of host names and ports from config file.
-    std::vector<std::tuple<const char*, const char*>> serverAddresses;
-
-    std::ifstream configFile(CONFIG_FILE_PATH);
-    if (!configFile) {
-      std::cout << "cannot open config file" << std::endl;
-      return 1;
-    }
-
-    char buf[MAX_CONFIG_LINE_LEN];
-    while (configFile) {
-      configFile.getline(buf, MAX_CONFIG_LINE_LEN);
-      std::string line(buf);
-      if (configFile) {
-        int pos = line.find_first_of(' ');
-        auto host = line.substr(0, pos);
-        auto port = line.substr(pos + 1);
-        serverAddresses.push_back(std::make_tuple(host.c_str(), port.c_str()));
-      }
-    }
-
-    // Parse operations from command argument.
-    char* unparsed_ops = argv[1];
-    std::vector<std::string> parsed_ops;
-
-    char *saveptr;  // Used for thread safety by strtok_r.
-    char* token = strtok_r(unparsed_ops, ",", &saveptr);
-
-    while (token != NULL) {
-      parsed_ops.push_back(std::string(token));
-      token = strtok_r(NULL, ",", &saveptr);
-    }
-
-    // For now, print ops to cout. Eventually, they will be printed
-    // to an output file after completion.
-    for (int i = 0; i < parsed_ops.size(); i++) {
-      std::cout << parsed_ops[i] << std::endl;
-    }
-
-    boost::asio::io_context io_context;
-    tcp::resolver resolver(io_context);
-
-    // Note: Only connect to the first server in our config, for now.
-    auto host = std::get<0>(serverAddresses[0]);
-    auto port = std::get<1>(serverAddresses[0]);
-    tcp::resolver::results_type endpoints = resolver.resolve(host, port);
-
-    tcp::socket socket(io_context);
-    boost::asio::connect(socket, endpoints);
-
-    std::cout << socket.remote_endpoint().port() << std::endl;
-
-    for (;;) {
-      char buf[MAX_SOCKET_DATA_LEN];
-      boost::system::error_code error;
-
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-      if (error == boost::asio::error::eof) {
-        // Connection closed cleanly.
-        break;
-      } else if (error) {
-        // Some other error.
-        throw boost::system::system_error(error);
-      }
-
-      Message m;
-      std::string archive_data(buf, len);
-      std::istringstream archive_stream(archive_data);
-      boost::archive::text_iarchive archive(archive_stream);
-      archive >> m;
-      std::cout << m.GetMessage() << std::endl;
-    }
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-}
-*/
