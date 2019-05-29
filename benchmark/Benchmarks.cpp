ecdee0a5e971a7056b3617ee29b700e0968d1237
Justin Johnson <justinthomasjohnson@gmail.com>
2019-05-29 12:43:59 -0700

    Add test placeholders for UDP/TCP Mencius. (#55)

--- benchmark/Benchmarks.cpp
+++ benchmark/Benchmarks.cpp
@@ -1,18 +1,103 @@
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
+/* Constants */
+const int kNumReplicas[] = {3, 5};
+const int kNumClients[] = {1, 2, 3};
+const int kDropRates[] = {0, 1, 5};
+const int kDropRateDenom = 1000; 
+
+/* Benchmark Fixtures */
+
+class TCPMenciusFixture : public benchmark::Fixture {
+ public:
+  void SetUp(const ::benchmark::State &state) {
+  }
+
+  void TearDown(const ::benchmark::State &state) {
+  }
+};
+
+class UDPMenciusFixture : public benchmark::Fixture {
+ public:
+  void SetUp(const ::benchmark::State &state) {
+  }
+
+  void TearDown(const ::benchmark::State &state) {
+  }
+};
+
+/* Benchmark Arguments */
+
+static void TestArguments(benchmark::internal::Benchmark *b) {
+  for (int num_replicas : kNumReplicas) {
+    for (int num_clients : kNumClients) {
+      for (int drop_rate : kDropRates) {
+        b->Args({num_replicas, num_clients, drop_rate});
+      }
+    }
+  }
+}
+
+/* Benchmarks */
+
+BENCHMARK_DEFINE_F(TCPMenciusFixture, NoFailures)(benchmark::State &state) {
+  auto num_replicas = state.range(0);
+  auto num_clients = state.range(1);
+  auto drop_rate = state.range(2) / kDropRateDenom;
+
   for (auto _ : state)
     std::string empty_string;
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK_REGISTER_F(TCPMenciusFixture, NoFailures)->Apply(TestArguments);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
+BENCHMARK_DEFINE_F(TCPMenciusFixture, GradualFailures)(benchmark::State &state) {
+  auto num_replicas = state.range(0);
+  auto num_clients = state.range(1);
+  auto drop_rate = state.range(2) / kDropRateDenom;
+
   for (auto _ : state)
-    std::string copy(x);
+    std::string empty_string;
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK_REGISTER_F(TCPMenciusFixture, GradualFailures)->Apply(TestArguments);
+
+BENCHMARK_DEFINE_F(TCPMenciusFixture, InstantFailures)(benchmark::State &state) {
+  auto num_replicas = state.range(0);
+  auto num_clients = state.range(1);
+  auto drop_rate = state.range(2) / kDropRateDenom;
+
+  for (auto _ : state)
+    std::string empty_string;
+}
+BENCHMARK_REGISTER_F(TCPMenciusFixture, InstantFailures)->Apply(TestArguments);
+
+BENCHMARK_DEFINE_F(UDPMenciusFixture, NoFailures)(benchmark::State &state) {
+  auto num_replicas = state.range(0);
+  auto num_clients = state.range(1);
+  auto drop_rate = state.range(2) / kDropRateDenom;
+
+  for (auto _ : state)
+    std::string empty_string;
+}
+BENCHMARK_REGISTER_F(UDPMenciusFixture, NoFailures)->Apply(TestArguments);
+
+BENCHMARK_DEFINE_F(UDPMenciusFixture, GradualFailures)(benchmark::State &state) {
+  auto num_replicas = state.range(0);
+  auto num_clients = state.range(1);
+  auto drop_rate = state.range(2) / kDropRateDenom;
+
+  for (auto _ : state)
+    std::string empty_string;
+}
+BENCHMARK_REGISTER_F(UDPMenciusFixture, GradualFailures)->Apply(TestArguments);
+
+BENCHMARK_DEFINE_F(UDPMenciusFixture, InstantFailures)(benchmark::State &state) {
+  auto num_replicas = state.range(0);
+  auto num_clients = state.range(1);
+  auto drop_rate = state.range(2) / kDropRateDenom;
+
+  for (auto _ : state)
+    std::string empty_string;
+}
+BENCHMARK_REGISTER_F(UDPMenciusFixture, InstantFailures)->Apply(TestArguments);
 
 BENCHMARK_MAIN();
